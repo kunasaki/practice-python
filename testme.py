ab5d5033447ed7e6e3fdf8bded74b03fe7c4bcca
kunasaki <brahamortal@gmail.com>
2019-03-09 15:33:56 +0800

    modify content

--- testme.py
+++ testme.py
@@ -1,6 +1,12 @@
-class test:
+class mytest():
     def __init__(self,name):
 	    print('test init')
 	    self.name=name
     def haha(self):
 	    print(self.name)
+		
+class myclass:
+    "A simple example class"
+    i = 12345
+    def f(x):
+        return 'hello world'
